c6c6636b22aaf42da380e370816546efdaa1af89
asd1asd00000 <126613068+asd1asd00000@users.noreply.github.com>
2026-08-07 18:25:03 +0330

    Add Order struct for database representation

---
+++ models/order.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+// Order ساختار فاکتور در دیتابیس را مشخص می‌کند
+type Order struct {
+	ID           int       `json:"id"`
+	TrackingCode string    `json:"tracking_code"`
+	PlanName     string    `json:"plan_name"`
+	BasePrice    int       `json:"base_price"`
+	UniqueAmount int       `json:"unique_amount"`
+	Status       string    `json:"status"` // pending یا paid
+	ConfigLink   string    `json:"config_link"`
+	CreatedAt    time.Time `json:"created_at"`
+}
