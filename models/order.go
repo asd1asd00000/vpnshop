605b3b57f24525908d4b12a74a46c3e1de1c4db2
asd1asd00000 <126613068+asd1asd00000@users.noreply.github.com>
2026-09-04 22:21:46 +0330

    Update order.go

--- models/order.go
+++ models/order.go
@@ -4,14 +4,20 @@ import "time"
 
 // Order ساختار فاکتور در دیتابیس را مشخص می‌کند
 type Order struct {
-	ID           int       `json:"id"`
-	TrackingCode string    `json:"tracking_code"`
-	PlanName     string    `json:"plan_name"`
-	BasePrice    int       `json:"base_price"`
-	UniqueAmount int       `json:"unique_amount"`
-	Status       string    `json:"status"` // pending یا paid
-	ConfigLink   string    `json:"config_link"`
-	CreatedAt    time.Time `json:"created_at"`
-	RenewUsername string `json:"renew_username"`
-	CarryGB       int    `json:"carry_gb"`
+	ID             int    `json:"id"`
+	TrackingCode   string `json:"tracking_code"`
+	PlanName       string `json:"plan_name"`
+	BasePrice      int    `json:"base_price"`
+	UniqueAmount   int    `json:"unique_amount"`
+	Status         string `json:"status"`
+	ConfigLink     string `json:"config_link"`
+	AdminConfirmed bool   `json:"admin_confirmed"`
+	PaymentMethod  string `json:"payment_method"`
+	CreatedAt      string `json:"created_at"`
+	PaidAt         string `json:"paid_at"`
+	AdminNote      string `json:"admin_note"`
+	Archived       int    `json:"archived"`
+	PlanDays       int    `json:"plan_days"`
+	RenewUsername  string `json:"renew_username"`
+	CarryGB        int    `json:"carry_gb"`
 }
